59a5829713c3911d7f67275f5a58dc40f9abbe8e
Dilan Laptop <dilangarcia145@gmail.com>
2026-02-21 14:27:08 -0600

    Initial commit

---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home Controller</title>
+    <link rel="stylesheet" href="{{ asset('build/css/home.css') }}">
+</head>
+<body>
+    <section class="main-content">
+        <div class="container">
+            <h1>Titulo 1</h1>
+        </div>
+        <div class="container">
+            <h2>Titulo 2</h2>
+        </div>
+        <div class="container">
+            <h3>Titulo 3</h3>
+        </div>
+
+    </section>
+    <section>
+        <h1>Hola</h1>
+        <div class="container">
+            <h3>Titulo 3</h3>
+        </div>
+    </section>
+
+
+</body>
+</html>
