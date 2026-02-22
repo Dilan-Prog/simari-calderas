4b771e8e46de97c2716220ab92ccf9eca7bc1dbb
Dilan Laptop <dilangarcia145@gmail.com>
2026-02-21 23:12:38 -0600

    2do commit

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -8,24 +8,36 @@
     <link rel="stylesheet" href="{{ asset('build/css/home.css') }}">
 </head>
 <body>
-    <section class="main-content">
-        <div class="container">
-            <h1>Titulo 1</h1>
-        </div>
-        <div class="container">
-            <h2>Titulo 2</h2>
-        </div>
-        <div class="container">
-            <h3>Titulo 3</h3>
-        </div>
+    <header>
 
-    </section>
-    <section>
-        <h1>Hola</h1>
-        <div class="container">
-            <h3>Titulo 3</h3>
-        </div>
-    </section>
+    </header>
+    <main>
+        <section class="section-1">
+            <div class="container">
+                <h1>Titulo 1</h1>
+            </div>
+            <div class="container">
+                <h2>Titulo 2</h2>
+            </div>
+            <div class="container" >
+                <h3>Titulo 3</h3>
+            </div>
+        </section>
+         <section>
+            <h1>Hola</h1>
+            <div class="container">
+                <h3>Titulo 3</h3>
+            </div>
+        </section>
+    </main>
+        <section>
+            <h1>Hola</h1>
+            <div class="container">
+                <h1>jaime gey</h1>
+                <h2>jaime gey</h2>
+                <h3>jaime gey</h3>
+            </div>
+        </section>
 
 
 </body>
