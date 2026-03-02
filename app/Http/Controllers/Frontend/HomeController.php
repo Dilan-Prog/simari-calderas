8987780f94435fd1d32e83841e9de0889bdae8c7
lucks187 <lucks2872@gmail.com>
2026-03-01 23:43:18 -0600

    first structure improvement

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -10,6 +10,6 @@ class HomeController extends Controller
     //
     public function index()
     {
-        return view('frontend.pages.products.services.chillers_main');
+        return view('frontend.pages.products.services.preventive_main');
     }
 }
