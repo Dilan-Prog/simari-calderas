7bd8ca01b11ecb8a451e055bcc62134c30efffec
Dilan-Prog <dilanp270105@gmail.com>
2026-03-09 10:52:09 -0600

    Istalacion de etiquetas Google

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -5,6 +5,16 @@
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <!-- SEO BASICS -->
     <title>Calderas industriales y mantenimiento 24/7 | SIMARI Calderas</title>
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-QRCNC1DEYM"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-QRCNC1DEYM');
+    </script>
     <meta
       name="description"
       content="Diseño, instalación y mantenimiento de calderas industriales. Auditorías de eficiencia energética, soporte 24/7 y cumplimiento NOM/ASME."
@@ -98,26 +108,24 @@
 
     <!-- (Opcional) Performance -->
     <meta name="robots" content="index,follow" />
+    <!-- Google Tag Manager -->
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-PLSDTJHX');</script>
+    <!-- End Google Tag Manager -->
+    
   </head>
   <body>
+    <!-- Google Tag Manager (noscript) -->
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PLSDTJHX"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     @include('frontend.layouts.header')
     <main>
         @yield('content')
-
-
-
-
-
-
-
-
-
-
-
-
-
     </main>
-
     @include('frontend.layouts.footer')
     <script>
       document.getElementById("year").textContent = new Date().getFullYear();
