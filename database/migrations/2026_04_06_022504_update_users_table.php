1e5f53f9d4c1c9dc4d0ec8a63074e9e338c397c9
Dilan-Prog <dilanp270105@gmail.com>
2026-04-05 20:54:09 -0600

    Update table Users

---
+++ database/migrations/2026_04_06_022504_update_users_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('first_name', 100)->after('id');
+            $table->string('last_name', 100)->after('first_name');
+            $table->string('position', 150)->after('last_name');
+            $table->string('avatar_url', 255)->after('password')->nullable();
+            $table->string('phone', 30)->after('email');
+            $table->enum('status', ['active', 'inactive', 'suspended'])->after('avatar_url')->default('inactive');
+            $table->string('rfc', 15)->after('status');
+            $table->string('curp', 18)->after('rfc')->nullable();
+            $table->string('social_segurity_number', 20)->after('curp')->nullable();
+            $table->date('birthdate')->after('social_segurity_number')->nullable();
+            $table->integer('id_contact_emergency')->after('birthdate')->nullable();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn([
+                'first_name',
+                'last_name',
+                'position',
+                'avatar_url',
+                'phone',
+                'status',
+                'rfc',
+                'curp',
+                'social_segurity_number',
+                'birthdate',
+                'id_contact_emergency'
+            ]);
+        });
+    }
+};
