dd936dadd27f1f585f53c017427cbeb524707da9
Dilan Laptop <dilangarcia145@gmail.com>
2026-05-05 13:18:44 -0600

    chenges to equiterm main-equiterm

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -106,7 +106,7 @@
                 C
               </a>
             </div>
-            <div class="mega-col">
+            <div class="mega-col">ull
               <h4>Filtros de Agua Industriales</h4>
               <a href="{{ route('hydraulic-engineering') }}">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><circle cx="12" cy="12" r="10"></circle><path d="m16.24 7.76-1.804 5.411a2 2 0 0 1-1.265 1.265L7.76 16.24l1.804-5.411a2 2 0 0 1 1.265-1.265z"></path></svg>
