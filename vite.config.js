231566a097a8565e4ab71e6993c313fe2fbbe22c
Dilan Laptop <dilangarcia145@gmail.com>
2026-06-25 08:58:27 -0600

    ultimate V

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,7 @@ export default defineConfig({
                 'resources/css/admin/technical-services.css',
                 'resources/js/admin/technical-services.js',
                 'resources/css/service-reports.css',
+                
             ],
             refresh: true,
         }),
