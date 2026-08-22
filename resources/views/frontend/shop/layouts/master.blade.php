86603da37e7a5f1c2365e331554992edca5ef7ef
Dilan Laptop <dilangarcia145@gmail.com>
2026-08-22 16:33:46 -0600

    Instala Google Tag Manager (GTM-MXFK4BK9) en el sitio público
    Script en el <head> lo más arriba posible y el <noscript> justo
    después de abrir <body>, exactamente como pide Google. Solo en el
    layout del storefront (frontend.shop.layouts.master) -- cubre home,
    catálogo, producto, checkout y páginas legales; no se agrega al admin
    ni al portal de clientes.

--- resources/views/frontend/shop/layouts/master.blade.php
+++ resources/views/frontend/shop/layouts/master.blade.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html lang="es-MX">
   <head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-MXFK4BK9');</script>
+    <!-- End Google Tag Manager -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @include('frontend.layouts.partials.head-meta')
     {{--
@@ -11,6 +18,10 @@
     @vite(array_merge(['resources/js/frontend/shop/ad-tracking.js'], $shopVite ?? []))
   </head>
   <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MXFK4BK9"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 
     @include('frontend.shop.layouts.header')
     <main>
